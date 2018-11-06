e23caca486ca2d2c43b9f2353a729ca434f1d34c
Valentin LECOQ-VALLON <vlecoq-v@e2r2p1.42.fr>
2018-11-06 12:36:06 +0100

    ex13 norm

--- piscinereloaded/ex13/ft_recursive_factorial.c
+++ piscinereloaded/ex13/ft_recursive_factorial.c
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlecoq-v <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 12:27:05 by vlecoq-v          #+#    #+#             */
+/*   Updated: 2018/11/06 12:35:33 by vlecoq-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
-		return(0);
-	printf("MAMEN ON EST A %d\n", nb);
-	if(nb - 1 <=  0)
-		return(1);
-	return(nb*ft_recursive_factorial(nb - 1));	
+		return (0);
+	if (nb > 12)
+		return (0);
+	if (nb - 1 <= 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
 }
