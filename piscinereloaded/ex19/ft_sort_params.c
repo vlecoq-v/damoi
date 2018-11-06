8922d996e867a125afb0a2bdb360206076633179
Valentin LECOQ-VALLON <vlecoq-v@e2r11p21.42.fr>
2018-11-06 15:46:11 +0100

    ex19 a la norme

--- piscinereloaded/ex19/ft_sort_params.c
+++ piscinereloaded/ex19/ft_sort_params.c
@@ -1,19 +1,30 @@
-#include <stdio.h>
-#include "ft_putchar.c"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlecoq-v <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 14:42:50 by vlecoq-v          #+#    #+#             */
+/*   Updated: 2018/11/06 15:45:10 by vlecoq-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ft_strcmp(char *s1, char *s2)
+int		ft_putchar(char c);
+
+int		ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	while (s1[i + 1] != '\0' && s2[j + 1] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[j] != '\0')
 	{
 		i++;
 		j++;
 	}
-	return(s1[i] - s2[j]);
+	return (s1[i] - s2[j]);
 }
 
 void	ft_putstr(char *str)
@@ -21,41 +32,29 @@ void	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
-	while(str[i] != 0)
+	while (str[i] != 0)
 	{
 		ft_putchar(str[i]);
 		i++;
 	}
 }
 
-void	ft_swtch_str(char *s1, char *s2)
+int		main(int argc, char **argv)
 {
-	char *swtch;
-
-	swtch = s1;
-	s1 = s2;
-	s2 = swtch;
-}
-
-int	main(int argc, char **argv)
-{
-	int	i;
+	int		i;
 	char	*swtch;
 
-	i = 0;
-	while (++i < argc - 2)
+	i = 1;
+	while (i < argc - 1)
 	{
-		printf("WHILE \n");
 		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
 		{
-			printf("ENVOIE SWITCH %d et %d \n", i, i + 1 );
-	//		ft_swtch_str(argv[i], argv[i + 1]);
 			swtch = argv[i];
 			argv[i] = argv[i + 1];
 			argv[i + 1] = swtch;
-			printf("S1 = %s et S2 = %s \n", argv[i], argv[i +1]);
-			i = 0;
+			i = 1;
 		}
+		i++;
 	}
 	i = 1;
 	while (i < argc)
@@ -64,5 +63,5 @@ int	main(int argc, char **argv)
 		i++;
 		ft_putchar('\n');
 	}
-	return(0);
+	return (0);
 }
