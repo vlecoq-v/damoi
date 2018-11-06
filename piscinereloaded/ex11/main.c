3875a36aabdffd6fde35d2bcdfb4619be87bbf7a
Valentin LECOQ-VALLON <vlecoq-v@e2r11p21.42.fr>
2018-11-06 13:52:16 +0100

    bien gerer la valeur 0

--- piscinereloaded/ex11/main.c
+++ piscinereloaded/ex11/main.c
@@ -10,6 +10,8 @@ int	main ()
 
 	a = 7;
 	b = 0;
+	pt_div = 0;
+	pt_mod = 0;
 	printf("VALEUR DE A = %d, VALEUR DE B = %d\n", a, b);
 	ft_div_mod(a, b, &pt_div, &pt_mod);
 	printf("VALEUR DE DIV = %d, VALEUR DE MOD = %d\n", pt_div, pt_mod);
