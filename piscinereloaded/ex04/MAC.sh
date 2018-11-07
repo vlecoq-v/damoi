be8a1ab9a74364b722987b8e38715288cdde5738
Valentin LECOQ-VALLON <vlecoq-v@e2r13p12.42.fr>
2018-11-07 14:51:39 +0100

    suppression d'un espace a la fin

--- piscinereloaded/ex04/MAC.sh
+++ piscinereloaded/ex04/MAC.sh
@@ -6,8 +6,8 @@
 #    By: vlecoq-v <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/11/06 10:47:51 by vlecoq-v          #+#    #+#              #
-#    Updated: 2018/11/06 10:51:00 by vlecoq-v         ###   ########.fr        #
+#    Updated: 2018/11/07 14:51:16 by vlecoq-v         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-ifconfig | sed -n '/ether/ p' | cut -c8-
+ifconfig | sed -n '/ether/ p' | cut -c8- | rev | cut -c2- | rev
