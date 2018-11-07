08476ffc211dfdce2f0346afa08814d2814afd0c
Valentin LECOQ-VALLON <vlecoq-v@e3r7p17.42.fr>
2018-11-07 13:24:09 +0100

    amelioration ex27

--- piscinereloaded/ex27/main.c
+++ piscinereloaded/ex27/main.c
@@ -6,34 +6,47 @@
 /*   By: vlecoq-v <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 11:50:42 by vlecoq-v          #+#    #+#             */
-/*   Updated: 2018/11/07 12:11:02 by vlecoq-v         ###   ########.fr       */
+/*   Updated: 2018/11/07 13:16:45 by vlecoq-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
+int	arg_error(int argc)
+{
+	if (argc != 2)
+	{
+		if (argc > 2)
+			write(2, "Too many arguments.\n", 20);
+		if (argc < 2)
+			write(2, "File name missing.\n", 19);
+		return (0);
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	char	buf[200];
+	char	buf[1000];
 	int		nread;
 	int		fd;
 
-	if (argc > 2)
+	nread = 1;
+	if (arg_error(argc) != 1)
+		return (0);
+	if ((fd = open(argv[1], O_RDONLY)) < 0)
 	{
-		write(2, "Too many arguments.\n", 20);
+		write(2, "Open error.\n", 14);
 		return (0);
 	}
-	if (argc < 2)
+	while (nread != 0)
 	{
-		write(2, "File name missing.\n", 19);
-		return (0);
+		nread = read(fd, buf, 1000);
+		if (nread == -1)
+			write(2, "Read error\n", 13);
+		if (write(1, buf, nread) != nread)
+			write(2, "Write error\n", 14);
 	}
-	fd = open(argv[1], O_RDONLY);
-	nread = read(fd, buf, 200);
-	if (nread == -1 || nread == 0)
-		write(2, "Read error\n", 13);
-	if (write(1, buf, nread) != nread)
-		write(2, "Write error'n", 14);
 	close(fd);
 	return (0);
 }
